a79d440291415c40c9f4d68baadebf8de305f3b1
HYH <huyuhao612@outlook.com>
2026-05-31 20:32:44 +0800

    2026-05-31 项目初始化

---
+++ src/main/java/space/huyuhao/myoj/MyOjApplication.java
@@ -0,0 +1,13 @@
+package space.huyuhao.myoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/space/huyuhao/myoj/MyOjApplicationTests.java
@@ -0,0 +1,13 @@
+package space.huyuhao.myoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
