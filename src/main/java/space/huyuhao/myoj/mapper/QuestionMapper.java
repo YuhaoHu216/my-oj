72e088cb92e4d62394eae7bbc1f470e05e2d5e9a
HYH <huyuhao612@outlook.com>
2026-07-13 23:33:31 +0800

    2026-07-13 添加题目与提交 Mapper 接口

---
+++ src/main/java/space/huyuhao/myoj/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package space.huyuhao.myoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import space.huyuhao.myoj.entity.Question;
+
+/**
+ * 题目 Mapper
+ */
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
---
+++ src/main/java/space/huyuhao/myoj/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,11 @@
+package space.huyuhao.myoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import space.huyuhao.myoj.entity.QuestionSubmit;
+
+/**
+ * 题目提交 Mapper
+ */
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
