8f410fd17a5b0fd0ef2b6c727b31b11c88c4b071
HYH <huyuhao612@outlook.com>
2026-07-13 23:34:29 +0800

    2026-07-13 添加题目与提交 Controller 层（CRUD + 权限控制 + 代码脱敏）

---
+++ src/main/java/space/huyuhao/myoj/controller/QuestionController.java
@@ -0,0 +1,212 @@
+package space.huyuhao.myoj.controller;
+
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import space.huyuhao.myoj.annotation.AuthCheck;
+import space.huyuhao.myoj.common.DeleteRequest;
+import space.huyuhao.myoj.common.ErrorCode;
+import space.huyuhao.myoj.common.ResponseResult;
+import space.huyuhao.myoj.constant.UserConstant;
+import space.huyuhao.myoj.context.UserContext;
+import space.huyuhao.myoj.entity.Question;
+import space.huyuhao.myoj.entity.User;
+import space.huyuhao.myoj.entity.dto.question.*;
+import space.huyuhao.myoj.entity.vo.QuestionVO;
+import space.huyuhao.myoj.exception.BusinessException;
+import space.huyuhao.myoj.exception.ThrowUtils;
+import space.huyuhao.myoj.service.QuestionService;
+import space.huyuhao.myoj.service.UserService;
+
+import java.util.List;
+
+/**
+ * 题目接口
+ */
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 创建
+     */
+    @PostMapping("/add")
+    public ResponseResult<Long> addQuestion(@RequestBody QuestionAddRequest questionAddRequest) {
+        if (questionAddRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = new Question();
+        BeanUtils.copyProperties(questionAddRequest, question);
+        List<String> tags = questionAddRequest.getTags();
+        if (tags != null) {
+            question.setTags(JSONUtil.toJsonStr(tags));
+        }
+        List<JudgeCase> judgeCase = questionAddRequest.getJudgeCase();
+        if (judgeCase != null) {
+            question.setJudgeCase(JSONUtil.toJsonStr(judgeCase));
+        }
+        JudgeConfig judgeConfig = questionAddRequest.getJudgeConfig();
+        if (judgeConfig != null) {
+            question.setJudgeConfig(JSONUtil.toJsonStr(judgeConfig));
+        }
+        questionService.validQuestion(question, true);
+        User loginUser = userService.getById(UserContext.getUserId());
+        question.setUserId(loginUser.getId());
+        question.setFavourNum(0);
+        question.setThumbNum(0);
+        boolean result = questionService.save(question);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResponseResult.success(question.getId());
+    }
+
+    /**
+     * 删除
+     */
+    @PostMapping("/delete")
+    public ResponseResult<Boolean> deleteQuestion(@RequestBody DeleteRequest deleteRequest) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User user = userService.getById(UserContext.getUserId());
+        long id = deleteRequest.getId();
+        Question oldQuestion = questionService.getById(id);
+        ThrowUtils.throwIf(oldQuestion == null, ErrorCode.NOT_FOUND_ERROR);
+        if (!oldQuestion.getUserId().equals(user.getId()) && !userService.isAdmin(user)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        return ResponseResult.success(questionService.removeById(id));
+    }
+
+    /**
+     * 更新（仅管理员）
+     */
+    @PostMapping("/update")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public ResponseResult<Boolean> updateQuestion(@RequestBody QuestionUpdateRequest questionUpdateRequest) {
+        if (questionUpdateRequest == null || questionUpdateRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = new Question();
+        BeanUtils.copyProperties(questionUpdateRequest, question);
+        List<String> tags = questionUpdateRequest.getTags();
+        if (tags != null) {
+            question.setTags(JSONUtil.toJsonStr(tags));
+        }
+        List<JudgeCase> judgeCase = questionUpdateRequest.getJudgeCase();
+        if (judgeCase != null) {
+            question.setJudgeCase(JSONUtil.toJsonStr(judgeCase));
+        }
+        JudgeConfig judgeConfig = questionUpdateRequest.getJudgeConfig();
+        if (judgeConfig != null) {
+            question.setJudgeConfig(JSONUtil.toJsonStr(judgeConfig));
+        }
+        questionService.validQuestion(question, false);
+        long id = questionUpdateRequest.getId();
+        Question oldQuestion = questionService.getById(id);
+        ThrowUtils.throwIf(oldQuestion == null, ErrorCode.NOT_FOUND_ERROR);
+        return ResponseResult.success(questionService.updateById(question));
+    }
+
+    /**
+     * 根据 id 获取
+     */
+    @GetMapping("/get/vo")
+    public ResponseResult<QuestionVO> getQuestionVOById(long id) {
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = questionService.getById(id);
+        if (question == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        return ResponseResult.success(questionService.getQuestionVO(question));
+    }
+
+    /**
+     * 分页获取列表（封装类）
+     */
+    @PostMapping("/list/page/vo")
+    public ResponseResult<Page<QuestionVO>> listQuestionVOByPage(
+            @RequestBody QuestionQueryRequest questionQueryRequest) {
+        long current = questionQueryRequest.getCurrent();
+        long size = questionQueryRequest.getPageSize();
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Question> questionPage = questionService.page(new Page<>(current, size),
+                questionService.getQueryWrapper(questionQueryRequest));
+        return ResponseResult.success(questionService.getQuestionVOPage(questionPage));
+    }
+
+    /**
+     * 分页获取当前用户创建的资源列表
+     */
+    @PostMapping("/my/list/page/vo")
+    public ResponseResult<Page<QuestionVO>> listMyQuestionVOByPage(
+            @RequestBody QuestionQueryRequest questionQueryRequest) {
+        if (questionQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getById(UserContext.getUserId());
+        questionQueryRequest.setUserId(loginUser.getId());
+        long current = questionQueryRequest.getCurrent();
+        long size = questionQueryRequest.getPageSize();
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Question> questionPage = questionService.page(new Page<>(current, size),
+                questionService.getQueryWrapper(questionQueryRequest));
+        return ResponseResult.success(questionService.getQuestionVOPage(questionPage));
+    }
+
+    /**
+     * 分页获取题目列表（仅管理员）
+     */
+    @PostMapping("/list/page")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public ResponseResult<Page<Question>> listQuestionByPage(
+            @RequestBody QuestionQueryRequest questionQueryRequest) {
+        long current = questionQueryRequest.getCurrent();
+        long size = questionQueryRequest.getPageSize();
+        Page<Question> questionPage = questionService.page(new Page<>(current, size),
+                questionService.getQueryWrapper(questionQueryRequest));
+        return ResponseResult.success(questionPage);
+    }
+
+    /**
+     * 编辑（用户）
+     */
+    @PostMapping("/edit")
+    public ResponseResult<Boolean> editQuestion(@RequestBody QuestionEditRequest questionEditRequest) {
+        if (questionEditRequest == null || questionEditRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = new Question();
+        BeanUtils.copyProperties(questionEditRequest, question);
+        List<String> tags = questionEditRequest.getTags();
+        if (tags != null) {
+            question.setTags(JSONUtil.toJsonStr(tags));
+        }
+        List<JudgeCase> judgeCase = questionEditRequest.getJudgeCase();
+        if (judgeCase != null) {
+            question.setJudgeCase(JSONUtil.toJsonStr(judgeCase));
+        }
+        JudgeConfig judgeConfig = questionEditRequest.getJudgeConfig();
+        if (judgeConfig != null) {
+            question.setJudgeConfig(JSONUtil.toJsonStr(judgeConfig));
+        }
+        questionService.validQuestion(question, false);
+        User loginUser = userService.getById(UserContext.getUserId());
+        long id = questionEditRequest.getId();
+        Question oldQuestion = questionService.getById(id);
+        ThrowUtils.throwIf(oldQuestion == null, ErrorCode.NOT_FOUND_ERROR);
+        if (!oldQuestion.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        return ResponseResult.success(questionService.updateById(question));
+    }
+}
---
+++ src/main/java/space/huyuhao/myoj/controller/QuestionSubmitController.java
@@ -0,0 +1,58 @@
+package space.huyuhao.myoj.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import space.huyuhao.myoj.common.ErrorCode;
+import space.huyuhao.myoj.common.ResponseResult;
+import space.huyuhao.myoj.context.UserContext;
+import space.huyuhao.myoj.entity.QuestionSubmit;
+import space.huyuhao.myoj.entity.User;
+import space.huyuhao.myoj.entity.dto.questionsubmit.QuestionSubmitAddRequest;
+import space.huyuhao.myoj.entity.dto.questionsubmit.QuestionSubmitQueryRequest;
+import space.huyuhao.myoj.entity.vo.QuestionSubmitVO;
+import space.huyuhao.myoj.exception.BusinessException;
+import space.huyuhao.myoj.service.QuestionSubmitService;
+import space.huyuhao.myoj.service.UserService;
+
+/**
+ * 题目提交接口
+ */
+@RestController
+@RequestMapping("/question_submit")
+public class QuestionSubmitController {
+
+    @Autowired
+    private QuestionSubmitService questionSubmitService;
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 提交题目
+     */
+    @PostMapping("/")
+    public ResponseResult<Long> doQuestionSubmit(
+            @RequestBody QuestionSubmitAddRequest questionSubmitAddRequest) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        final User loginUser = userService.getById(UserContext.getUserId());
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResponseResult.success(questionSubmitId);
+    }
+
+    /**
+     * 分页获取题目提交列表（脱敏）
+     */
+    @PostMapping("/list/page")
+    public ResponseResult<Page<QuestionSubmitVO>> listQuestionSubmitByPage(
+            @RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        final User loginUser = userService.getById(UserContext.getUserId());
+        return ResponseResult.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
+    }
+}
