9fc2554571b6bd595fc9d5023e6d7fa5a76bbf50
HYH <huyuhao612@outlook.com>
2026-07-13 23:33:14 +0800

    2026-07-13 添加实体类与视图对象（Question, QuestionSubmit, UserVO, QuestionVO, QuestionSubmitVO）

---
+++ src/main/java/space/huyuhao/myoj/entity/vo/QuestionSubmitVO.java
@@ -0,0 +1,56 @@
+package space.huyuhao.myoj.entity.vo;
+
+import cn.hutool.json.JSONUtil;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+import space.huyuhao.myoj.dto.UserVO;
+import space.huyuhao.myoj.entity.QuestionSubmit;
+import space.huyuhao.myoj.entity.dto.questionsubmit.JudgeInfo;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 题目提交封装类
+ */
+@Data
+public class QuestionSubmitVO implements Serializable {
+
+    private Long id;
+    private String language;
+    private String code;
+    private JudgeInfo judgeInfo;
+    private Integer status;
+    private Long questionId;
+    private Long userId;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    private UserVO userVO;
+    private QuestionVO questionVO;
+
+    public static QuestionSubmit voToObj(QuestionSubmitVO questionSubmitVO) {
+        if (questionSubmitVO == null) {
+            return null;
+        }
+        QuestionSubmit questionSubmit = new QuestionSubmit();
+        BeanUtils.copyProperties(questionSubmitVO, questionSubmit);
+        JudgeInfo judgeInfoObj = questionSubmitVO.getJudgeInfo();
+        if (judgeInfoObj != null) {
+            questionSubmit.setJudgeInfo(JSONUtil.toJsonStr(judgeInfoObj));
+        }
+        return questionSubmit;
+    }
+
+    public static QuestionSubmitVO objToVo(QuestionSubmit questionSubmit) {
+        if (questionSubmit == null) {
+            return null;
+        }
+        QuestionSubmitVO questionSubmitVO = new QuestionSubmitVO();
+        BeanUtils.copyProperties(questionSubmit, questionSubmitVO);
+        String judgeInfoStr = questionSubmit.getJudgeInfo();
+        questionSubmitVO.setJudgeInfo(JSONUtil.toBean(judgeInfoStr, JudgeInfo.class));
+        return questionSubmitVO;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/space/huyuhao/myoj/entity/vo/QuestionVO.java
@@ -0,0 +1,65 @@
+package space.huyuhao.myoj.entity.vo;
+
+import cn.hutool.json.JSONUtil;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+import space.huyuhao.myoj.dto.UserVO;
+import space.huyuhao.myoj.entity.Question;
+import space.huyuhao.myoj.entity.dto.question.JudgeConfig;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 题目封装类
+ */
+@Data
+public class QuestionVO implements Serializable {
+
+    private Long id;
+    private String title;
+    private String content;
+    private List<String> tags;
+    private Integer submitNum;
+    private Integer acceptedNum;
+    private JudgeConfig judgeConfig;
+    private Integer thumbNum;
+    private Integer favourNum;
+    private Long userId;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    private UserVO userVO;
+
+    public static Question voToObj(QuestionVO questionVO) {
+        if (questionVO == null) {
+            return null;
+        }
+        Question question = new Question();
+        BeanUtils.copyProperties(questionVO, question);
+        List<String> tagList = questionVO.getTags();
+        if (tagList != null) {
+            question.setTags(JSONUtil.toJsonStr(tagList));
+        }
+        JudgeConfig voJudgeConfig = questionVO.getJudgeConfig();
+        if (voJudgeConfig != null) {
+            question.setJudgeConfig(JSONUtil.toJsonStr(voJudgeConfig));
+        }
+        return question;
+    }
+
+    public static QuestionVO objToVo(Question question) {
+        if (question == null) {
+            return null;
+        }
+        QuestionVO questionVO = new QuestionVO();
+        BeanUtils.copyProperties(question, questionVO);
+        List<String> tagList = JSONUtil.toList(question.getTags(), String.class);
+        questionVO.setTags(tagList);
+        String judgeConfigStr = question.getJudgeConfig();
+        questionVO.setJudgeConfig(JSONUtil.toBean(judgeConfigStr, JudgeConfig.class));
+        return questionVO;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
